a54672282e76b175c36acb016d46a9cc01ab0f5e
phu.huynh <phu.huynh@vn.snapresearch.io>
2024-04-23 17:31:32 +0700

    * deploy: Hela Mainnet

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -15,4 +15,5 @@ export const WETH9: { [chainId: number]: Token } = {
   [42161]: new Token(42161, '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', 18, 'WETH', 'Wrapped Ether'),
   [421611]: new Token(421611, '0xB47e6A5f8b33b3F17603C83a0535A9dcD7E32681', 18, 'WETH', 'Wrapped Ether'),
   [666888]: new Token(666888, '0xc544f0b5C0259172b6D6a1f6394aB21B5fd8F4Bf', 18, 'WHLUSD', 'Wrapped HLUSD'),
+  [8668]: new Token(8668, '0x3a035615e101373FA9BA21c5bEa7FE4026fc40b4', 18, 'WHLUSD', 'Wrapped HLUSD'),
 }
